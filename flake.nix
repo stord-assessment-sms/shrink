0e189aceda99ff90d2f1a6361eff7c8dbb4e4842
Shane Sveller <shanesveller@gmail.com>
2024-04-09 08:00:52 -0500

    feat(workspace): Initialize Nix devShell

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Link Shortener";
+
+  inputs = {
+    beam-flakes = {
+      url = "github:shanesveller/nix-beam-flakes";
+      inputs.flake-parts.follows = "flake-parts";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = inputs @ {
+    beam-flakes,
+    flake-parts,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [beam-flakes.flakeModule];
+
+      systems = ["aarch64-darwin" "x86_64-darwin" "x86_64-linux"];
+
+      perSystem = _: {
+        beamWorkspace = {
+          enable = true;
+          devShell = {
+            languageServers.elixir = true;
+            languageServers.erlang = false;
+            phoenix = true;
+          };
+          versions = {
+            elixir = "1.16.2";
+            erlang = "26.2.3";
+          };
+        };
+      };
+    };
+}
