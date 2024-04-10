edcc83ab0f40acb344c243cfcd4ac812b7a4d6f9
Shane Sveller <shanesveller@gmail.com>
2024-04-10 14:26:44 -0500

    feat(ui): Add copy-to-clipboard UI elements

--- assets/js/app.js
+++ assets/js/app.js
@@ -34,6 +34,10 @@ let liveSocket = new LiveSocket("/live", Socket, {
 topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" });
 window.addEventListener("phx:page-loading-start", (_info) => topbar.show(300));
 window.addEventListener("phx:page-loading-stop", (_info) => topbar.hide());
+window.addEventListener("phx:copy-to-clipboard", (event) => {
+  event.preventDefault();
+  navigator.clipboard.writeText(event.target.dataset.copy);
+});
 
 // connect if there are any LiveViews on the page
 liveSocket.connect();
