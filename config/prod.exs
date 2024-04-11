c92190bfa291f1a1eb857d36eb63e5a5395d64be
Shane Sveller <shanesveller@gmail.com>
2024-04-11 09:14:23 -0500

    chore(deploy): Use JSON logs format in production envs

--- config/prod.exs
+++ config/prod.exs
@@ -8,7 +8,13 @@ import Config
 config :shrink, ShrinkWeb.Endpoint, cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
-config :logger, level: :info
+config :logger,
+  backends: [LoggerJSON],
+  level: :info
+
+config :logger_json, :backend,
+  metadata: :all,
+  json_encoder: Jason
 
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
